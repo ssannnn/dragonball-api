0955bb7eb09af2f03a344077564fec81d618b2d7
santiaguito-g <santi.gnzlz@protonmail.com>
2022-11-17 19:20:18 -0300

    Add schema & model for the characters

---
+++ src/models/characters.model.ts
@@ -0,0 +1,29 @@
+import { Schema, Types, model, Model } from "mongoose";
+import { Character } from "../interface/character.interface";
+
+const CharacterSchema = new Schema<Character>(
+    {
+        "id": {
+            type: String,
+        },
+        "name":{
+            type: String,
+        },
+        "power_level":{
+            type: String,
+        }, 
+        "saga":{
+            type: String,
+        }, 
+        "series":{
+            type: String,
+        }, 
+        "species":{
+            type: String,
+        } 
+    }   
+);
+
+const characterModel = model("characters", CharacterSchema, "dragonball-api");
+
+export default characterModel;
