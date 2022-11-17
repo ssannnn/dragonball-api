ed9a2bcc419f215106364f15721c8c8907412c13
santiaguito-g <santi.gnzlz@protonmail.com>
2022-11-17 17:15:11 -0300

    Add db connection

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import { router } from "./routes";
+import db from "./config/mongo"
 
 const PORT = process.env.PORT || 4001;
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(cors());
 
 app.use(router);
+
+db().then(() => console.log("Connection established"));
 
 app.listen(PORT, () => console.log(`Conectado en el puerto ${PORT}`));
 
