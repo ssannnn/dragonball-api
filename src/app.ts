87807030785a911dcad7a89212a0957c7e8dee73
Santiago Gonzalez <santi.gnzlz@protonmail.com>
2022-12-13 09:27:15 -0300

    Add missing semicolons

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import "dotenv/config"
-import express from "express"
-import cors from "cors"
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
 import { router } from "./routes";
-import db from "./config/mongo"
-import path from "path"
+import db from "./config/mongo";
+import path from "path";
 
 const PORT = process.env.PORT || 4001;
 
