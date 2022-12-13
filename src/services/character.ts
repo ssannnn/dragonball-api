b390ecc2d2f437579263bf51771332daea17c95f
Santiago Gonzalez <santi.gnzlz@protonmail.com>
2022-12-13 14:46:52 -0300

    Add custom labels to the pagination info

--- src/services/character.ts
+++ src/services/character.ts
@@ -5,10 +5,21 @@ const getCharacters = async (req:Request) => {
         
     const page: number = parseInt(req.query.page as string, 10) || 1;
 
+    const myCustomLabels = {
+        totalDocs: "charactersCount",
+        docs: "charactersList",
+        limit: "charactersPerPage",
+        page: "currentPage",
+        pagingCounter: "firstCharacterInThisPage",
+        nextPage: "next",
+        prevPage: "prev",
+    };
+    
     const options = {
         select: ("-_id"),
         page: page,
         limit: 10,
+        customLabels: myCustomLabels
     };
       
     const responseCharacter = await characterModel.paginate({}, options);
@@ -17,7 +28,7 @@ const getCharacters = async (req:Request) => {
 }
 
 const getCharacter = async (id:string) => {
-    const responseCharacter = await characterModel.findOne({ id: id });
+    const responseCharacter = await characterModel.findOne({ id: id }, "-_id");
     return responseCharacter;
 }
 
